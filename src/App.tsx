24a800d5184a418a45262d5ea9d06114bb240814
damirmamian <damirmamian@gmail.com>
2026-02-01 20:38:53 +0200

    done>

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from "./App.module.css"
+import { useState } from 'react';
+import CafeInfo from "./components/CafeInfo/CafeInfo"
+import VoteOptions from "./components/VoteOptions/VoteOptions.tsx";
+import type { Votes, VoteType } from "./types/votes.ts"
+import VoteStats from "./components/VoteStats/VoteStats.tsx";
+import { Notification } from "./components/Notification/Notification.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  function handleVote(type: VoteType) {
+    setVotes((vote => ({
+      ...vote,
+      [type]: vote[type] + 1
+    })))
+  }
+
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    })
+
+  }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes > 0
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0} />
+      {totalVotes > 0 && <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />}
+      {totalVotes == 0 && <Notification />}
+    </div>
   )
 }
 
